37a57bb6e03af8613155d921feea9709142de3d3
Valery <Valery.Yafremau@itechart-group.com>
2025-01-02 13:44:31 +0400

    Update boot.sh

--- Scripts/boot.sh
+++ Scripts/boot.sh
@@ -1,3 +1,3 @@
 cd /home/ubuntu/fr2 &&
 docker system prune -a --volumes --force &&
-COMPOSE_HTTP_TIMEOUT=200  docker compose -f docker-compose.yml up -d
+COMPOSE_HTTP_TIMEOUT=200  docker compose -f docker compose.yml up -d
